ed0859c328ba7bdf9d234d9d895ff55166454d66
ivogfonseca <ivinhospo@yahoo.com.br>
2024-06-03 20:05:50 -0300

    metodos classe cliente e alguns metodos classe conta

--- Banco/Conta.java
+++ Banco/Conta.java
@@ -1,3 +1,69 @@
 public class Conta {
+    private int numeroAgencia;
+    private int numeroConta;
+    private double saldo;
+    private Cliente cliente;
     
-}
+    
+    public Conta(int numeroAgencia, int numeroConta, double saldo, Cliente cliente) {
+        this.numeroAgencia = numeroAgencia;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    public int getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+
+    public void setNumeroAgencia(int numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void deposito(double quantia) {
+        if (quantia > 0) {
+            saldo += quantia;
+            System.out.println("Depósito de R$" + quantia);
+            System.out.println("Novo saldo: R$" + saldo);
+        }
+    }
+    
+    public void saque(double quantia){
+        if(quantia > 0 && saldo >= quantia){
+            saldo-= quantia;
+            System.out.println("Saque de R$" + quantia );
+            System.out.println("Novo saldo: R$" + saldo);
+        }
+    }
+}    
