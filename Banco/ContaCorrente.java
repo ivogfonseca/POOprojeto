39b5396d6fdb65b24fda92d7e827ca7d0c5b2123
ivogfonseca <ivinhospo@yahoo.com.br>
2024-06-05 11:23:52 -0300

    conta corrente

--- Banco/ContaCorrente.java
+++ Banco/ContaCorrente.java
@@ -1,3 +1,21 @@
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
+    public double taxaManutencao = 50;
+    
+    public ContaCorrente(int numeroAgencia, int numeroConta, double saldo, Cliente cliente) {
+        super(numeroAgencia, numeroConta, saldo, cliente);
+        //TODO Auto-generated constructor stub
+    }
+
+    public double simularOperacao(int meses){
+        double saldoM = getSaldo();
+        for(int i = 0; i < meses; i++){
+            saldoM -= taxaManutencao;
+            if(saldoM < 0){
+                saldoM = 0;
+                break;
+            }
+        }
+        return saldoM;
+    }
     
 }
